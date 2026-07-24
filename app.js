1946183db0ac2391fbb2ec2c2ac5d6d986ac7072
Bhagyalakshmi Anna <bhagyalakshmi.anna@gmail.com>
2026-07-24 14:14:36 +0530

    spliied app and server js

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-// Importing required packages
-const http = require('http');
 const express = require('express');
-
 const app = express();
 
-app.set('port', process.env.PORT || 3000); // Application port is set
-app.set('views', __dirname + '/app/server/views'); // Views folder is set
-app.set('view engine', 'ejs'); // View engine is set
-app.use(express.static(__dirname + '/app/public')); // Public folder containing static files is set
+app.get('/', (req, res) => {
+	res.status(200).send('Hello from Sample Node Project');
+});
 
-require('./app/routes')(app); // Routes are imported
-
-const server = http.createServer(app).listen(app.get('port'), function(){
-	console.log('The application is running on port ' + app.get('port'));
-}); // Http server is created
-
-module.exports = server;
+module.exports = app;
